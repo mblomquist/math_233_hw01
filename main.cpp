0759727fb4b05b247ac8a068e9469ebeee0ee637
mblomquist <Fr@$mb0627!>
2022-08-30 19:58:00 -0700

    Add function for problem 4b.

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ void problem_2() {
 // Dynamic allocation of a three-dimensional array
 void problem_3c() {
 // Define my_array
-    double*** my_array = new double**[M];
+    auto*** my_array = new double**[M];
     for (int i = 0; i < M; i++){
         my_array[i] = new double*[N];
         for (int j = 0; j < N; j++){
@@ -43,8 +43,20 @@ void problem_3c() {
 }
 
 // Problem 4b solution
-void problem_4b(){
+double Legendre(double x, int n){
 
+    if (n < 0 || n > 5)
+        throw std::invalid_argument("expected 0 <= n <= 5");
+
+    switch (n){
+        case 0 : return 1.;
+        case 1 : return x;
+        case 2 : return 0.5 * (3.*x*x - 1.);
+        case 3 : return 0.5 * (5.*x*x*x - 3.*x);
+        case 4 : return (1./8.) * (35.*x*x*x*x - 30.*x*x + 3.);
+        case 5 : return (1./8.) * (63.*x*x*x*x*x - 70.*x*x*x + 15.*x);
+        default : return 0.;
+    }
 }
 
 void problem_4c(){
@@ -63,9 +75,22 @@ int main() {
     std::cout << std::endl;
 
     // Problem 4b solution.
+    double test_4b_00, test_4b_01, test_4b_02, test_4b_03, test_4b_04, test_4b_05;
+
     std::cout << "Problem 4b solution:" << std::endl;
-    problem_4b();
-    std::cout << std::endl;
+    test_4b_00 = Legendre(0.5, 0);
+    test_4b_01 = Legendre(-0.4, 1);
+    test_4b_02 = Legendre(0.3, 2);
+    test_4b_03 = Legendre(-0.6, 3);
+    test_4b_04 = Legendre(0.7, 4);
+    test_4b_05 = Legendre(-0.8, 5);
+
+    std::cout << test_4b_00 << std::endl;
+    std::cout << test_4b_01 << std::endl;
+    std::cout << test_4b_02 << std::endl;
+    std::cout << test_4b_03 << std::endl;
+    std::cout << test_4b_04 << std::endl;
+    std::cout << test_4b_05 << std::endl;
 
     // Problem 4c solution.
     std::cout << "Problem 4c solution:" << std::endl;
